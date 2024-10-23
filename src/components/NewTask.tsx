77c6af829321640f874c7ab600e573f9031e2cb1
Luiz Neto <luizneton13@gmail.com>
2024-10-22 21:53:27 -0300

    adicionando funções para salvar as tarefas no localStorage e para pegar as tarefas salvas, adicionando tambem um id padrão caso ainda não exista nenhuma tarefa

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -3,18 +3,22 @@ import { dataType } from "../interfaces"
 import { KeyboardEvent } from "react"
 export default function NewTask() {
     const { task, setTask } = useTaskContext()
-    if(!task) return
+    if (!task) return
     function addNewTask(event: KeyboardEvent<HTMLInputElement>) {
-        if(event.key != "Enter") return
+        if (event.key != "Enter") return
         const eTarget = event.target as HTMLInputElement
         const allTasks = [...task["unChecked"], ...task["checked"]]
-        const newId = allTasks.reduce((total, currentValue): dataType => {
-            total = total.id > currentValue.id ? total : currentValue;
-            return total
-        })
+        let newId: number = 0
+        if (allTasks.length != 0)
+            newId = allTasks.reduce((total, currentValue): dataType => {
+                total = total.id > currentValue.id ? total : currentValue;
+                return total
+            }).id
+
+
         setTask({
             unChecked: [...task.unChecked, {
-                id: newId.id + 1,
+                id: newId + 1,
                 text: eTarget.value,
                 checked: false
             }],
@@ -22,7 +26,7 @@ export default function NewTask() {
         })
         eTarget.value = ""
     }
-   
+
     return (
         <div className="newTask">
             <div>
@@ -30,7 +34,7 @@ export default function NewTask() {
                     <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
                 </svg>
-                <input type="text" name="newTask" onKeyDown={(event: KeyboardEvent<HTMLInputElement>) => addNewTask(event)}/>
+                <input type="text" name="newTask" onKeyDown={(event: KeyboardEvent<HTMLInputElement>) => addNewTask(event)} />
             </div>
         </div>
     )
--- src/context/TaskContext.tsx
+++ src/context/TaskContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, ReactNode, useState } from "react";
+import { createContext, useContext, ReactNode, useState, useEffect } from "react";
 import { dataType } from "../interfaces";
 
 interface TaskContextProps {
@@ -15,25 +15,19 @@ const context = createContext({} as contextType)
 
 export function TaskContext({ children }: TaskContextProps) {
     const [data, setData] = useState({
-        unChecked: [
-            {
-                id: 1,
-                text: "tarefa",
-                checked: false
-            }
-        ],
-        checked: [
-            {
-                id: 2,
-                text: "tarefa1",
-                checked: true
-            }
-        ]
+        unChecked: [],
+        checked: []
     } as listTaskType)
-
+    function reloadTasks(){
+        const RLTask = localStorage.getItem("dataTask")
+        if(RLTask){
+            setData(JSON.parse(RLTask))
+        }
+    }
+    useEffect(()=>{reloadTasks()},[])
     function setTask(data: listTaskType): void {
         setData(data)
-        console.log(data)
+        localStorage.setItem("dataTask" ,JSON.stringify(data))
     }
     return (
         <context.Provider value={{ task: data, setTask }}>
